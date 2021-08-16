b520c3d49110fbfb22d3c00086ed8e0ca820e536
Aibek Kubanychbek uulu <dadavai_9494@gmail.com>
2021-08-16 23:53:42 +0600

    js commit modal
    Signed-off-by: Aibek Kubanychbek uulu <dadavai_9494@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const body = document.querySelector (".conteiner__body");
+const modal = document.querySelector (".modal");
+const openBtn = document.querySelector (".btn");
+const messeng = document.querySelector (".modal__form_btn");
+
+function openModal () {
+  modal.style.display = "block";
+}
+
+function closeModal () {
+  modal.style.display = "none";
+}
+
+openBtn.addEventListener("click", openModal);
+close.onclick = closeModal;
+
+window.onclick = function(event) {
+  if(event.target == body){
+    console.log("close body");
+    closeModal();
+  } else if(event.target == modal) {
+    console.log("close modal");
+    closeModal();
+  }
+}
